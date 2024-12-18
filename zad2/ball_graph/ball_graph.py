d66ad2a4c52484e4cd3278f08fd216c875b69160
Tomasz N <troll.yuck@gmail.com>
2024-12-18 18:26:11 +0100

    MPIS update, now with big balls

--- zad2/ball_graph/ball_graph.py
+++ zad2/ball_graph/ball_graph.py
@@ -39,15 +39,17 @@ yu_mean = np.array([np.mean(yu[x == unique]) for unique in unique_x])
 yd_c = yd - yc
 yd_c_mean = np.array([np.mean(yd_c[x == unique]) for unique in unique_x])
 
+#print(yb_mean)
+
 ################################
-'''
+
 #podpunkt a
 a1_graph = np.divide(yb_mean, unique_x)
 a2_graph = np.divide(yb_mean, np.sqrt(unique_x))
 
 #podpunkt b
 b1_graph = np.divide(yu_mean, unique_x)
-
+'''
 #podpunkt c
 c1_graph = np.divide(yc_mean, unique_x)
 c2_graph = np.divide(yc_mean, np.multiply(unique_x, np.log(unique_x)))
@@ -57,29 +59,30 @@ c3_graph = np.divide(yc_mean, np.power(unique_x, 2))
 d1_graph = np.divide(yd_mean, unique_x)
 d2_graph = np.divide(yd_mean, np.multiply(unique_x, np.log(unique_x)))
 d3_graph = np.divide(yd_mean, np.power(unique_x, 2))
-'''
+
 #podpunkt e
 e1_graph = np.divide(yd_c_mean, unique_x)
 e2_graph = np.divide(yd_c_mean, np.multiply(unique_x, np.log(unique_x)))
 e3_graph = np.divide(yd_c_mean, np.multiply(unique_x, np.log(np.log(unique_x))))
 
 ################################
-
+'''
 plt.figure(figsize=(16,11),dpi=300)
 plt.xlabel('n')
 plt.ylabel('M')
-plt.yscale('linear') #dobór skali
-plt.title('Podpunkt E (Skala liniowa)') #dobór tytułu
+plt.yscale('log') #dobór skali
+plt.xscale('log')
+plt.title('Podpunkt B') #dobór tytułu
 
-#plt.scatter(x, yd_c, label='Dn - Cn', s = 5,  zorder = 1) #naniesienie punktów na wykres
+#plt.scatter(x, yb, label='Dn - Cn', s = 5,  zorder = 1) #naniesienie punktów na wykres
 
-plt.plot(unique_x, e1_graph, color='red', label='e(n)/n' , zorder = 2) #naniesienie funkcji na wykres
-plt.plot(unique_x, e2_graph, color='green', label='e(n)/(n ln n)' , zorder = 3)
-plt.plot(unique_x, e3_graph, color='blue', label='e(n)/(n ln ln n)' , zorder = 4)
+plt.plot(unique_x, a1_graph, color='red', label='b(n)/n' , zorder = 2) #naniesienie funkcji na wykres
+plt.plot(unique_x, a2_graph, color='green', label='b(n)/(sqrt(n))' , zorder = 3)
+#plt.plot(unique_x, e3_graph, color='blue', label='e(n)/(n ln ln n)' , zorder = 4)
 
 plt.minorticks_on() #włączenie podziałki pomocniczej
 plt.legend() #włączenie legendy
 plt.grid(True) #włączenie siatki
 
-plt.savefig('PdpE.png', format='png', dpi=300) #zapis wykresu do pliku
+#plt.savefig('PdpB.png', format='png', dpi=300, transparent=True) #zapis wykresu do pliku
 plt.show()
